bae1cc92a8aeecc29eb70200c7155589fe95e5b6
zzpanqing <zzpanqing.learning@gmail.com>
2025-11-03 22:59:56 +0100

    ubisoft 03/11/2025 12:17

--- Ubisoft.py
+++ Ubisoft.py
@@ -4,50 +4,75 @@ Ubisoft (-10,66% à 6,92 euros)
 L'action de l'éditeur de jeux vidéo a clôturé au plus bas depuis fin octobre 2012. Les investisseurs devront patienter jusqu'au 13 novembre pour connaître les résultats du premier semestre.
 
 chiffre
--Troisième éditeur mondial indépendant de jeux vidéo, 
--138 millions de joueurs uniques, 
--créé en 1986 et 
--détenant les marques phares 
-  Assassin’s Creed, 
-  Just Dance, 
-  Watch Dogs, 
-  The Division, 
-  Far Cry, 
-  For Honor, 
-  Tom Clancy, 
-  the Crew, 
-  Ghost Recon, 
-  Rainbow, 
-  Brawlhalla… ;
+AOF - EN SAVOIR PLUS
+Points clés
+
+
+
+- Troisième éditeur mondial indépendant de jeux vidéo, attirant 138 millions de joueurs uniques, créé en 1986 et détenant les marques phares Assassin’s Creed, Just Dance, Watch Dogs, The Division, Far Cry, For Honor, Tom Clancy, the Crew, Ghost Recon, Rainbow, Brawlhalla… ;
 
 - Chiffre d'affaires de 2,3 Mds€ réalisé en Amérique du nord pour 53 % et en Europe pour 35 % ;
 
 - Activité répartie entre les consoles pour 56 %, les ordinateurs pour 32 % et les téléphones pour 6 % ;
 
-- Capital détenu à 25,42 % % du capital et 
-29,64 % des droits par 2 actionnaires agissant de concert
-  -les frères fondateurs et la famille Guillemot avec 15,48 % des actions d’une part, 
-  le chinois Tencent avec 9,99 % d’autre part- 
-  puis par les salariés (3,6 %) ;
+- 5 ambitions : détention de la totalité des marques, intégration des nouveautés technologiques dans la politique R&D, maîtrise de la production interne à 96 %, puis montée en puissance de la récurrence des revenus (64 % apportés par le « back catalogue ») et de la rentabilité via la digitalisation ;
+
+- Capital détenu à 25,42 % % du capital et 29,64 % des droits par 2 actionnaires agissant de concert -les frères fondateurs et la famille Guillemot avec 15,48 % des actions d’une part, le chinois Tencent avec 9,99 % d’autre part- puis par les salariés (3,6 %) ;
 
 - Présidence et direction générale du conseil de 11 membres assurée par Yves Guillemot ;
 
-enjeux
-  - réorganisation industrielle avec une réduction nette de 300 M€ de la base de coûts fixes entre 2024 et 2027, par fermeture de bureaux et sites de production,  baisse des effectifs, suppressions de jeux & ventes d’actifs non-essentiels, construction de marques et de services Live plus puissants, sélectivité accrue, au prix de dépréciations dans la R&D, et intégration des avis des joueurs via Title Updates,
-  - réorganisation capitalistique : création, d’ici fin 2025, d'une filiale dédiée aux marques emblématiques Assassin’s Creed, Far Cry & Tom Clancy’s et Rainbow Six et accueillant dans son capital, à hauteur de 25 %, son partenaire capitalistique chinois Tencent,
-  - valorisation des jeux et franchises via les partenariats et monétisation des droits de streaming,
-  - innovation fondée sur des technologies de pointe (Anvil et Snowdrop pour les moteurs, Ubisoft Connect et I3D.net pour la distribution), Voxel et cloud computing avec Scalar…, accélérée par le soutien aux start-ups -6 à 10 incubées chaque année- et par « Lead associate » (studios de création) et Motion Pictures (animation et film) ;
-  - Stratégie environnementale :
-  - recours aux énergies renouvelables pour les centres de données français et canadiens,
-    - séquestration des émissions de GES et réduction constante des émissions par employé, (10,8 % en 2024 vs 2019) ;
-  - Vers une meilleure aisance financière dans le développement des jeux avec, à  la fin de l’année, l’investissement de 1,16 Md€ de Tencent dans la filiale de jeux emblématiques :
-  - Bilan renforcé : 1,7 Md€ de fonds propres, trésorerie de 1 Md€ environ et dette nette de 885 M€.
+
+Enjeux
+
+
+
+- Agilité du modèle d’affaires, en refonte depuis un an, visant une nouvelle organisation d’ici la fin de l’année :
+
+
+         - réorganisation industrielle avec une réduction nette de 300 M€ de la base de coûts fixes entre 2024 et 2027, par fermeture de bureaux et sites de production,  baisse des effectifs, suppressions de jeux & ventes d’actifs non-essentiels, construction de marques et de services Live plus puissants, sélectivité accrue, au prix de dépréciations dans la R&D, et intégration des avis des joueurs via Title Updates,
+
+
+
+
+      - réorganisation capitalistique : création, d’ici fin 2025, d'une filiale dédiée aux marques emblématiques Assassin’s Creed, Far Cry & Tom Clancy’s et Rainbow Six et accueillant dans son capital, à hauteur de 25 %, son partenaire capitalistique chinois Tencent,
+
+
+      - valorisation des jeux et franchises via les partenariats et monétisation des droits de streaming,
+
+
+      - innovation fondée sur des technologies de pointe (Anvil et Snowdrop pour les moteurs, Ubisoft Connect et I3D.net pour la distribution), Voxel et cloud computing avec Scalar…, accélérée par le soutien aux start-ups -6 à 10 incubées chaque année- et par « Lead associate » (studios de création) et Motion Pictures (animation et film) ;
+
+
+- Stratégie environnementale :
+
+      - recours aux énergies renouvelables pour les centres de données français et canadiens,
+
+
+      - séquestration des émissions de GES et réduction constante des émissions par employé, (10,8 % en 2024 vs 2019) ;
+
+- Vers une meilleure aisance financière dans le développement des jeux avec, à  la fin de l’année, l’investissement de 1,16 Md€ de Telcen dans la filiale de jeux emblématiques :
+
+
+- Bilan renforcé : 1,7 Md€ de fonds propres, trésorerie de 1 Md€ environ et dette nette de 885 M€.
+
+
+
+
 Défis
-  - Activité commerciale centrée sur septembre-janvier, d’où la clôture des comptes à fin mars ;
-  - Suivi du net booking, de 1,85 Md€, soit un repli de 20 % en 2024-25
-  - Retombées de l’engagement fort dans les jeux d’aventure en monde ouvert, avec objectif de regagner le leadership mondial, et dans les expériences natives ou GaaS ;
-  - Poursuite du beau succès du lancement en mars de Assassin’s Creed Shadows ;
-  - Après une forte perte nette sur l’exercice 2024-25, objectifs abaissés :
-    - 2025-26 : stabilité du net booking, résultat opérationnel autour de l’équilibre, autofinancement libre encore négatif et position de dette autour de zéro,
-    - 2025-26 : retour à la profitabilité opérationnelle et à la génération d’autofinancement libre et impact positif sur l’activité de nouveaux contenus ;
-  - Société ne distribuant pas de dividende.
+
+
+- Activité commerciale centrée sur septembre-janvier, d’où la clôture des comptes à fin mars ;
+
+- Suivi du net booking, de 1,85 Md€, soit un repli de 20 % en 2024-25
+
+- Retombées de l’engagement fort dans les jeux d’aventure en monde ouvert, avec objectif de regagner le leadership mondial, et dans les expériences natives ou GaaS ;
+
+- Poursuite du beau succès du lancement en mars de Assassin’s Creed Shadows ;
+
+- Après une forte perte nette sur l’exercice 2024-25, objectifs abaissés :
+
+         - 2025-26 : stabilité du net booking, résultat opérationnel autour de l’équilibre, autofinancement libre encore négatif et position de dette autour de zéro,
+
+         - 2025-26 : retour à la profitabilité opérationnelle et à la génération d’autofinancement libre et impact positif sur l’activité de nouveaux contenus ;
+
+- Société ne distribuant pas de dividende.
